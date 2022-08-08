3dce07c707f3d0d69f0275ff71b584cace04c4a9
Atharwa Kamble <atharwa.kamble24@gmail.com>
2022-08-08 16:39:16 +0530

    done

---
+++ app/src/main/java/com/myshoppal/ui/fragments/OrdersFragment.kt
@@ -0,0 +1,61 @@
+package com.myshoppal.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.myshoppal.R
+import com.myshoppal.firestore.FirestoreClass
+import com.myshoppal.models.Order
+import com.myshoppal.ui.adapters.MyOrdersListAdapter
+import kotlinx.android.synthetic.main.fragment_orders.*
+
+
+class OrdersFragment : BaseFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val root = inflater.inflate(R.layout.fragment_orders, container, false)
+        return root
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        getMyOrdersList()
+    }
+
+
+    private fun getMyOrdersList() {
+
+        showProgressDialog(resources.getString(R.string.please_wait))
+
+        FirestoreClass().getMyOrdersList(this@OrdersFragment)
+    }
+
+
+    fun populateOrdersListInUI(ordersList: ArrayList<Order>) {
+
+
+        hideProgressDialog()
+
+        if (ordersList.size > 0) {
+
+            rv_my_order_items.visibility = View.VISIBLE
+            tv_no_orders_found.visibility = View.GONE
+
+            rv_my_order_items.layoutManager = LinearLayoutManager(activity)
+            rv_my_order_items.setHasFixedSize(true)
+
+            val myOrdersAdapter = MyOrdersListAdapter(requireActivity(), ordersList)
+            rv_my_order_items.adapter = myOrdersAdapter
+        } else {
+            rv_my_order_items.visibility = View.GONE
+            tv_no_orders_found.visibility = View.VISIBLE
+        }
+    }
+}
